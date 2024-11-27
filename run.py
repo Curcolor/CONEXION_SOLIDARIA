8fbee3fd91ae1252473ff74b149cb055f3f028d9
JUNIORRDSR <solano200801@gmail.com>
2024-11-27 04:32:38 -0500

    cambios doc

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
-from src import app
+from src import app, db
 import os
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5001)))
+    with app.app_context():
+        db.create_all()  # Crea todas las tablas en la base de datos
+        app.run(debug=True)
--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__,static_url_path='/src/static/', static_folder='static')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tu_base_de_datos.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///donaciones.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 from src.routes import *
