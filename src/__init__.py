6e41cf5cc253d888aee009677b4ac9ba92b79eb5
Curcolor <jairoarcia2003@gmail.com>
2024-11-27 04:47:54 -0500

    commit jairo

--- src/__init__.py
+++ src/__init__.py
@@ -8,7 +8,6 @@ from .Models.modelo_db import BaseDatos
 app = Flask(__name__,static_url_path='/src/static/', static_folder='static')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///donaciones.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 app = Flask(
         __name__, static_url_path="/src/static/", static_folder="static"
     )
